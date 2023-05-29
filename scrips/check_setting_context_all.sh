3deb8bc7eb00a936a8ec87afa3a9c7cd2e8bad3a
PhuongNM <phuongnm.bkhn@gmail.com>
2023-05-29 16:25:58 +0900

    fix bug path

--- scrips/check_setting_context_all.sh
+++ scrips/check_setting_context_all.sh
@@ -13,24 +13,21 @@
 cd $PBS_O_WORKDIR
 
 source ~/.bashrc
+conda activate env_py38/  
 
-cd /home/phuongnm/per_erc/  && conda activate /home/phuongnm/deeplearning_tutorial/env_py38/  && \
-python /home/phuongnm/per_erc/main.py --intra_speaker_context \
+python src/main.py --intra_speaker_context \
 --dropout  0.2 --batch_size 1 --lr 1e-5 --max_ep 30 --froze_bert_layer 10 \
 --data_name_pattern "iemocap.{}window2.json" --pre_trained_model_name roberta-large
 
-cd /home/phuongnm/per_erc/  && conda activate /home/phuongnm/deeplearning_tutorial/env_py38/  && \
-python /home/phuongnm/per_erc/main.py --intra_speaker_context \
+python src/main.py --intra_speaker_context \
 --dropout  0.2 --batch_size 1 --lr 5e-6 --max_ep 30 --froze_bert_layer 10 \
 --data_name_pattern "meld.{}window2.json" --pre_trained_model_name roberta-large
 
-cd /home/phuongnm/per_erc/  && conda activate /home/phuongnm/deeplearning_tutorial/env_py38/  && \
-python /home/phuongnm/per_erc/main.py --intra_speaker_context \
+python src/main.py --intra_speaker_context \
 --dropout  0.2 --batch_size 1 --lr 5e-6 --max_ep 30 --froze_bert_layer 10 \
 --data_name_pattern "emorynlp.{}window2.json" --pre_trained_model_name roberta-large
 
-cd /home/phuongnm/per_erc/  && conda activate /home/phuongnm/deeplearning_tutorial/env_py38/  && \
-python /home/phuongnm/per_erc/main.py --intra_speaker_context  \
+python src/main.py --intra_speaker_context  \
 --dropout  0.2 --batch_size 1 --lr 5e-6 --max_ep 30 --froze_bert_layer 10 \
 --data_name_pattern "dailydialog.{}window2.json" --pre_trained_model_name roberta-large
 
